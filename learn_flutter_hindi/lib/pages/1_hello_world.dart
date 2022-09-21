dd41bd6cb2c7b31e329745edf08c1cb111abc981
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-21 21:53:53 +0530

    master

--- learn_flutter_hindi/lib/pages/1_hello_world.dart
+++ learn_flutter_hindi/lib/pages/1_hello_world.dart
@@ -3,15 +3,24 @@ import 'package:flutter/material.dart';
 class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Material(
-        color: Colors.cyan,
-        child: Center(
-            child: Text(
-              "Hello World", style: TextStyle(color: Colors.white),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.purpleAccent,
+            title: const Text("Home Screen"),
+          ),
+          body: const Material(
+            color: Colors.purple,
+            child: Center(
+                child: Text(
+              "Hello World",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
               textDirection: TextDirection.ltr,
             )),
-      ),
+          )),
     );
   }
 }
