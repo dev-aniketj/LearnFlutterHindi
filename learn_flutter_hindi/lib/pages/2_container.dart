4af58bed799ea1b9c215e91c72e8bbe2792edec1
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-22 14:48:27 +0530

    master

--- learn_flutter_hindi/lib/pages/2_container.dart
+++ learn_flutter_hindi/lib/pages/2_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Contain_2 extends StatelessWidget {
   @override
@@ -6,12 +7,12 @@ class Contain_2 extends StatelessWidget {
     return Container(
       alignment: Alignment.center,
       color: Colors.orange,
-      margin: const EdgeInsets.only(bottom: 200),
+      margin: const EdgeInsets.only(bottom: 100),
       padding: const EdgeInsets.only(top: 50, left: 100),
-      child: const Text(
-        "My name is Aniket Jain\nI am a new flutter developer.",
-        textDirection: TextDirection.ltr,
-      ),
+      child: Text("My name is Aniket Jain.\nA new flutter developer.",
+          textDirection: TextDirection.ltr,
+          style:
+              GoogleFonts.mansalva(textStyle: const TextStyle(fontSize: 24))),
     );
   }
 }
