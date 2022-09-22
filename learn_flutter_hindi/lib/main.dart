9e8b92e773c7cd2a1c37d609c63c39ec248fd6ab
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-22 13:46:19 +0530

    master

--- learn_flutter_hindi/lib/main.dart
+++ learn_flutter_hindi/lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'pages/1_hello_world.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // when user want to remove the debug banner.
+      debugShowCheckedModeBanner: false,
       title: 'MyApp',
       themeMode: ThemeMode.light,
       initialRoute: '1',
--- learn_flutter_hindi/lib/pages/1_hello_world.dart
+++ learn_flutter_hindi/lib/pages/1_hello_world.dart
@@ -4,6 +4,7 @@ class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.purpleAccent,
