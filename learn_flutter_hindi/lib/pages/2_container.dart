7134906c79969ed90498b3640b011c68a438669f
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-23 15:59:33 +0530

    master

--- learn_flutter_hindi/lib/pages/2_container.dart
+++ learn_flutter_hindi/lib/pages/2_container.dart
@@ -5,14 +5,22 @@ class Contain_2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      color: Colors.orange,
-      margin: const EdgeInsets.only(bottom: 100),
-      padding: const EdgeInsets.only(top: 50, left: 100),
-      child: Text("My name is Aniket Jain.\nA new flutter developer.",
-          textDirection: TextDirection.ltr,
-          style:
-              GoogleFonts.mansalva(textStyle: const TextStyle(fontSize: 24))),
-    );
+        alignment: Alignment.center,
+        color: Colors.orange,
+        // margin: const EdgeInsets.only(bottom: 100),
+        // padding: const EdgeInsets.only(top: 50, left: 100),
+        child: Column(
+          children: [
+            Text("My name is Aniket Jain.\nA new flutter developer.",
+                textDirection: TextDirection.ltr,
+                style: GoogleFonts.mansalva(
+                    textStyle: const TextStyle(fontSize: 24))),
+            Text(
+              "My age is 23 years.",
+              textDirection: TextDirection.ltr,
+              style: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 26)),
+            ),
+          ],
+        ));
   }
 }
