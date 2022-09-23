f0e9f03875ea04b57b1ecdbe2c5705f01323f23e
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-23 16:14:02 +0530

    master

--- learn_flutter_hindi/lib/pages/2_container.dart
+++ learn_flutter_hindi/lib/pages/2_container.dart
@@ -4,23 +4,56 @@ import 'package:google_fonts/google_fonts.dart';
 class Contain_2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        alignment: Alignment.center,
-        color: Colors.orange,
-        // margin: const EdgeInsets.only(bottom: 100),
-        // padding: const EdgeInsets.only(top: 50, left: 100),
-        child: Column(
-          children: [
-            Text("My name is Aniket Jain.\nA new flutter developer.",
-                textDirection: TextDirection.ltr,
-                style: GoogleFonts.mansalva(
-                    textStyle: const TextStyle(fontSize: 24))),
-            Text(
-              "My age is 23 years.",
-              textDirection: TextDirection.ltr,
-              style: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 26)),
-            ),
-          ],
-        ));
+    return Center(
+        child: Container(
+            padding: const EdgeInsets.only(left: 20, top: 40),
+            alignment: Alignment.center,
+            color: Colors.orange,
+            // margin: const EdgeInsets.only(bottom: 100),
+            // padding: const EdgeInsets.only(top: 50, left: 100),
+            child: Column(
+              children: [
+                Text("Sunday",
+                    textDirection: TextDirection.ltr,
+                    style: GoogleFonts.lato(
+                        textStyle: const TextStyle(fontSize: 24))),
+                Text(
+                  "Monday",
+                  textDirection: TextDirection.ltr,
+                  style: GoogleFonts.lato(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
+                Text(
+                  "Tuesday",
+                  textDirection: TextDirection.ltr,
+                  style: GoogleFonts.lato(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
+                Text(
+                  "Wednesday",
+                  textDirection: TextDirection.ltr,
+                  style: GoogleFonts.lato(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
+                Text(
+                  "Thursday",
+                  textDirection: TextDirection.ltr,
+                  style: GoogleFonts.lato(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
+                Text(
+                  "Friday",
+                  textDirection: TextDirection.ltr,
+                  style: GoogleFonts.lato(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
+                Text(
+                  "Saturday",
+                  textDirection: TextDirection.ltr,
+                  style: GoogleFonts.lato(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
+              ],
+            )));
   }
 }
