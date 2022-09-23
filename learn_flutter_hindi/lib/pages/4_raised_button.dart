343659672df62fa89515aa12e518ab92d482087c
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-23 21:08:12 +0530

    master

--- learn_flutter_hindi/lib/pages/4_raised_button.dart
+++ learn_flutter_hindi/lib/pages/4_raised_button.dart
@@ -12,7 +12,10 @@ class RaisedButton_4 extends StatelessWidget {
           width: 200.0,
           margin: const EdgeInsets.only(left: 50, top: 40),
           child: RaisedButton(
-            onPressed: () {},
+            onPressed: () {
+              // loginPage(context);
+              showAlertDialog(context);
+            },
             color: Colors.deepPurple[70],
             elevation: 4.0,
             child: Text(
@@ -24,4 +27,22 @@ class RaisedButton_4 extends StatelessWidget {
       ),
     );
   }
+}
+
+void showAlertDialog(BuildContext context) {
+  Widget cancelBtn = TextButton(onPressed: () {}, child: Text("Cancel"));
+  Widget continueBtn = TextButton(onPressed: () {}, child: Text("Continue"));
+
+  AlertDialog dialog = AlertDialog(
+    title: Text("Alert Dialog"),
+    content: Text(
+        "Would you like to continue learning how to use Flutter widgets ? "),
+    actions: [cancelBtn, continueBtn],
+  );
+
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return dialog;
+      });
 }
