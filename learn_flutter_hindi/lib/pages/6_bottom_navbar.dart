1e5691afa6dbf7b239d5f33e43104ce8adb2bc5e
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-24 19:25:30 +0530

    master

--- learn_flutter_hindi/lib/pages/6_bottom_navbar.dart
+++ learn_flutter_hindi/lib/pages/6_bottom_navbar.dart
@@ -10,18 +10,32 @@ class BottomNavBar_6 extends StatelessWidget {
         selectedItemColor: Colors.blue.shade500,
         items: [
           BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
+              icon: const Icon(Icons.favorite),
               label: "Favorite",
+              tooltip: 'Favorite Menu',
               backgroundColor: Colors.blue.shade300),
           BottomNavigationBarItem(
-              icon: Icon(Icons.home),
+              icon: const Icon(Icons.home),
               label: "Home",
+              tooltip: 'Home Menu',
               backgroundColor: Colors.blue.shade300),
           BottomNavigationBarItem(
-              icon: Icon(Icons.trending_up),
+              icon: const Icon(Icons.trending_up),
               label: "Trending",
+              tooltip: 'Trending Menu',
               backgroundColor: Colors.blue.shade300),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Tapped');
+        },
+        tooltip: "Search here",
+        backgroundColor: Colors.lightBlue.shade700,
+        child: Icon(
+          Icons.search,
+          color: Colors.white.withAlpha(240),
+        ),
       ),
     );
   }
