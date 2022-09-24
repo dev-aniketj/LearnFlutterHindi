2b2e7a136a776d4774eae27fdb1678d2c0bd7fd5
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-24 19:18:54 +0530

    master

--- learn_flutter_hindi/lib/pages/6_bottom_navbar.dart
+++ learn_flutter_hindi/lib/pages/6_bottom_navbar.dart
@@ -5,22 +5,22 @@ class BottomNavBar_6 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black.withAlpha(220),
+        unselectedItemColor: Colors.white.withAlpha(245),
+        selectedItemColor: Colors.blue.shade500,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: "Favorite",
-            backgroundColor: Colors.blue.shade300,
-            tooltip: "Hello World"
-          ),
+              icon: Icon(Icons.favorite),
+              label: "Favorite",
+              backgroundColor: Colors.blue.shade300),
           BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: "Home",
               backgroundColor: Colors.blue.shade300),
           BottomNavigationBarItem(
-            icon: Icon(Icons.trending_up),
-            label: "Trending",
-            backgroundColor: Colors.blue.shade300,
-          ),
+              icon: Icon(Icons.trending_up),
+              label: "Trending",
+              backgroundColor: Colors.blue.shade300),
         ],
       ),
     );
